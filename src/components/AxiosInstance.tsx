50ff66f886ac35923cb3c297ff2c225b22964966
Mikhail Makarov <mikhail@makarov.tech>
2023-01-06 18:47:49 +0000

    append ci

--- src/components/AxiosInstance.tsx
+++ src/components/AxiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://127.0.0.1:8080',
+  baseURL: 'http://ec2-13-48-28-156.eu-north-1.compute.amazonaws.com/api',
   timeout: 1000
 });
