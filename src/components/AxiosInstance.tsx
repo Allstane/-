6c4ea5d6e7588f826360ca7c9116f7fe0b8c78a3
Mikhail Makarov <mihailmakarov9959@gmail.com>
2023-01-08 13:01:15 +0000

    Update AxiosInstance.tsx

--- src/components/AxiosInstance.tsx
+++ src/components/AxiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://ec2-13-48-28-156.eu-north-1.compute.amazonaws.com/api',
+  baseURL: 'http://ec2-13-48-204-23.eu-north-1.compute.amazonaws.com/api',
   timeout: 1000
 });
