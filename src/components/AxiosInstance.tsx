a75676c5e3f29aae83a0841d41e061198b6708f7
Stanislav <142887480+stakap@users.noreply.github.com>
2023-09-03 19:37:07 +0300

    Update AxiosInstance.tsx

--- src/components/AxiosInstance.tsx
+++ src/components/AxiosInstance.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const instance = axios.create({
-  baseURL: 'http://0.0.0.0:80',
+  baseURL: 'https://lb-north-1c-235947014.eu-north-1.elb.amazonaws.com:80',
   timeout: 10000
 });
